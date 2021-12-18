0fc53221292a39894d0ce2f8a9e03987f98c1816
Scott Sullivan <scott@xrdev.com>
2021-12-18 15:37:33 -0600

    add contact page and connect to db

---
+++ components/contact/contact-form.js
@@ -0,0 +1,63 @@
+import { useRef } from 'react';
+
+import classes from './contact-form.module.css';
+
+const ContactForm = () => {
+	const emailInputRef = useRef();
+	const nameInputRef = useRef();
+	const messageInputRef = useRef();
+
+	const sendMessageHandler = (event) => {
+		event.preventDefault();
+
+		const enteredEmail = emailInputRef.current.value;
+		const enteredName = nameInputRef.current.value;
+		const enteredMessage = messageInputRef.current.value;
+
+		// TODO add client side validation
+
+		fetch('/api/contact', {
+			method: 'POST',
+			body: JSON.stringify({
+				email: enteredEmail,
+				name: enteredName,
+				message: enteredMessage,
+			}),
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		});
+	};
+
+	return (
+		<section className={classes.contact}>
+			<h1>How can I help you?</h1>
+			<form className={classes.form} onSubmit={sendMessageHandler}>
+				<div className={classes.controls}>
+					<div className={classes.control}>
+						<label htmlFor="email">Your Email</label>
+						<input type="email" id="email" ref={emailInputRef} required />
+					</div>
+					<div className={classes.control}>
+						<label htmlFor="name">Your Name</label>
+						<input type="text" id="name" ref={nameInputRef} required />
+					</div>
+				</div>
+				<div className={classes.control}>
+					<label htmlFor="message">Your Message</label>
+					<textarea
+						id="message"
+						rows="5"
+						ref={messageInputRef}
+						required
+					></textarea>
+				</div>
+				<div className={classes.actions}>
+					<button>Send Message</button>
+				</div>
+			</form>
+		</section>
+	);
+};
+
+export default ContactForm;
